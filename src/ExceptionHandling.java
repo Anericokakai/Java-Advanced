ae29dcc970ba6eb364b1bcc824e7171625a27a73
Anericokakai <challykiddoh@gmail.com>
2023-09-23 18:15:59 +0300

    ERROR HANDLING OF BOTH CHECKED AND UNCHECKED EXCEPTIONS

---
+++ src/ExceptionHandling.java
@@ -0,0 +1,15 @@
+public class ExceptionHandling {
+
+    public  static void main(String[] args){
+
+// DIVIDING A NUMBER BY ZERO GIVES YOU  ARITHMETIC EXCEPTION
+//        int data=100/0;
+//        System.out.println(data);
+
+
+//NULL POINTER EXCEPTION
+//        YOU CANT WORK WITH NULL VALUES IN JAVA
+        String a=null;
+        System.out.println( a.charAt(0) );
+    }
+}
