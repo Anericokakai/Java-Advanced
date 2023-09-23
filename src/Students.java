4c99b81d39a6efd317f8fb0f765c7d426d7ea0c2
Anericokakai <challykiddoh@gmail.com>
2023-09-23 16:55:46 +0300

    METHODS CALLING IN JAVA AND MODIFICATION OF OBJECTS USING METHODS

---
+++ src/Students.java
@@ -0,0 +1,47 @@
+public class Students {
+    public Students(String name, String adress, int age) {
+        this.name = name;
+        this.adress = adress;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    String name;
+    String adress;
+    int age;
+}
